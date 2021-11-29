956efc9fd9014206d7ff56ac427311f119605d70
RedMarkD <reinout.de.bleser@gmail.com>
2021-11-29 11:53:36 +0100

    basic elements added in HTML, select written in angular

---
+++ project-name/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { Friend } from './friend';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public languages = ['HTML', 'CSS', 'JS', 'Ruby', 'PHP', 'TypeScript']
+}
+
+Friend
---
+++ project-name/src/app/friend.ts
@@ -0,0 +1,12 @@
+export class Friend {
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: number;
+  constructor(firstname: string, lastname:string, email: string, phone: number) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.email= email;
+    this.phone = phone;
+  }
+}
